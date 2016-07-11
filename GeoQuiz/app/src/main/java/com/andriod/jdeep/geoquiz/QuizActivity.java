7c938c122f5e4c57d590e3786d72b181677aba19
Jglamour <jasdeepglamour@gmail.com>
2016-07-11 11:51:24 -0400

    Overrode lifecycle methods and added log listeners to each method.

--- GeoQuiz/app/src/main/java/com/andriod/jdeep/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/andriod/jdeep/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.andriod.jdeep.geoquiz;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
         import android.view.View;
         import android.widget.Button;
         import android.widget.TextClock;
@@ -11,11 +12,13 @@ package com.andriod.jdeep.geoquiz;
 
 public class QuizActivity extends AppCompatActivity
 {
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPreviousButton;
     private TextView mQuestionTextView;
+
 
     private Question[] mQuestionBank= new Question[]
             {
@@ -25,6 +28,7 @@ public class QuizActivity extends AppCompatActivity
                     new Question(R.string.question_americas, true),
                     new Question(R.string.question_asia, true)
             };
+
     private int mCurrentIndex = 0;
     //update question when the next button is clicked
     private void updateQuestion()
@@ -54,10 +58,10 @@ public class QuizActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-
 
         //define button
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -120,4 +124,38 @@ public class QuizActivity extends AppCompatActivity
         updateQuestion();
     }
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
 }
